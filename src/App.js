55bdff69e6865be5c83b9edad91e897bfc33b389
kristoffer petersen <krizzmp@gmail.com>
2019-02-09 22:08:47 +0100

    lots o'stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import { FaTwitter, FaGithub } from "react-icons/fa";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>KRISTOFFER PETERSEN</p>
+          <div className="sub-line">
+            <span className="add">humble </span>web developer
+            <span className="strike"> extraordinaire</span>
+          </div>
+          <div className="icon-row">
+            <div className="icon">
+              <a href={"https://github.com/krizzmp"}>
+                <FaGithub>github</FaGithub>
+              </a>
+            </div>
+            <div className="icon">
+              <a href={"https://twitter.com/krizzmp"}>
+                <FaTwitter>twitter</FaTwitter>
+              </a>
+            </div>
+          </div>
         </header>
       </div>
     );
