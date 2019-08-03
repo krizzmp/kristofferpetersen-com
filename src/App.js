9d0e589cfe0c3ca28cdaadb5f52491e261699ec9
kristoffer petersen <krizzmp@gmail.com>
2019-08-03 21:13:01 +0200

    backup before reinstall of macos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 import { FaTwitter, FaGithub } from "react-icons/fa";
+
+function PortfolioLink({ title, description, url }) {
+  return (
+    <a href={url} style={{alignSelf: "stretch"}}>
+      <div className="portfolio-feature">
+        <div className="img" />
+        <div className="desc">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <button>go</button>
+        </div>
+      </div>
+    </a>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>KRISTOFFER PETERSEN</p>
+          <h1>KRISTOFFER PETERSEN</h1>
           <div className="sub-line">
             <span className="add">humble </span>web developer
             <span className="strike"> extraordinaire</span>
@@ -23,6 +39,12 @@ class App extends Component {
               </a>
             </div>
           </div>
+          <h2>My Work</h2>
+          <PortfolioLink
+            title="hatch"
+            description="an app for collaboratively creating simple diagrams"
+            url="https://hatch.now.sh"
+          />
         </header>
       </div>
     );
